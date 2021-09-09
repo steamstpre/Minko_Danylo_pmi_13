3649a2b370b498447a54a7d1c37f4364d52849e4
Denial <denialwins6@gmail.com>
2021-09-09 22:44:47 +0300

    First practice task

---
+++  practic/first_task/main.py
@@ -0,0 +1,54 @@
+import itertools
+import numpy
+
+#генеруєм всі можливі комбінації до певної довжини к , тобто зі всіх елемнтів числення
+#і викикдуєм повторні елементи з тої таблиця істиності
+
+def check_n():
+    n = 0
+    while n <= 2:
+        n = int(input("your n: "))
+    return n
+def check_k():
+    k = 0
+    while k <= 2  or k >= 10:
+        k = int(input("your k: "))
+    return k
+
+k = 0
+n = 0
+
+while n+k <= 4 or n+k >= 10:
+    k = check_k()
+    n = check_n()
+
+
+
+def apply_k(k):#all avaluable value
+    index = 0
+    our_arr = []
+    while index < k:
+        our_arr.append(index)
+        index += 1
+    return our_arr
+
+table = list(itertools.product(apply_k(k), repeat=n))
+
+count_of_our_elements = 0
+for items in table:
+    print(items)
+
+def search_our_count_of_elem(table):
+    count_of_our_elements = 0
+    for item in table:
+        index = 0
+        while index < n:
+            if index + 1 < len(item) and item[index] == 0 and item[index + 1] == 0:
+                count_of_our_elements += 1
+                break
+            index += 1
+    return count_of_our_elements
+
+our_res =  search_our_count_of_elem(table)
+
+print(our_res)
