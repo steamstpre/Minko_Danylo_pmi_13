809624858fe932d26c9ec33c0c095e549fcfe33d
Denial <denialwins6@gmail.com>
2021-09-16 20:43:37 +0300

    First_home_task

---
+++ prectica/main.py
@@ -0,0 +1,70 @@
+
+# #генеруєм всі можливі комбінації до певної довжини к , тобто зі всіх елемнтів числення
+# #і викикдуєм повторні елементи з тої таблиця істиності
+#тобто генеруєм всі можливі випадки довжини н зі всіма значиннями від 0 до к
+
+import itertools
+import numpy
+
+#генеруєм всі можливі комбінації до певної довжини к , тобто зі всіх елемнтів числення
+#і викикдуєм повторні елементи з тої таблиця істиності
+#use test folder it s the same code
+
+def input_num():
+    try:
+        num = int(input('Your num:'))
+    except:
+        print('please try num')
+        return input_num()
+    return num
+
+def check_n():
+    n = 0
+    while n <= 2:
+        print("enter n: ")
+        n = input_num()
+    return n
+def check_k():
+    k = 0
+    while k <= 1  or k >= 10:
+        print("enter k: ")
+        k = input_num()
+    return k
+
+k = 0
+n = 0
+
+while n+k <= 4 or n+k >= 18:
+    k = check_k()
+    n = check_n()
+
+
+
+def apply_k(k):#all avaluable value
+    index = 0
+    our_arr = []
+    while index < k:
+        our_arr.append(index)
+        index += 1
+    return our_arr
+
+table = list(itertools.product(apply_k(k), repeat=n))
+
+count_of_our_elements = 0
+for items in table:
+    print(items)
+
+def search_our_count_of_elem(table):
+    count_of_our_elements = 0
+    for item in table:
+        index = 0
+        while index < n:
+            if index + 1 < len(item) and item[index] == 0 and item[index + 1] == 0:
+                count_of_our_elements += 1
+                break
+            index += 1
+    return count_of_our_elements
+
+our_res =  search_our_count_of_elem(table)
+
+print(our_res)
--- programing/First_home_task/main.py
+++ programing/First_home_task/main.py
@@ -1,21 +1,25 @@
-
-def input_num():
+def enter_num():#input num
     try:
         count_of_elements = int(input("number: "))
     except:
         print("please enter number:")
-        return input_num()
+        return enter_num()
     return count_of_elements
 
-def input_our_arr(count_of_elements , our_arr):
+count_of_elements = enter_num()
+
+def input_our_arr(count_of_elements , our_arr):#input elements
     index = 0
     our_element = 0
     while index < count_of_elements:
-        our_element = input_num()
+        # our_element = int(input('your num: '))
+        our_element = enter_num()
         our_arr.append(our_element)
         index += 1
 
-def algoritm(count_of_elements ,our_arr):
+
+
+def algoritm(count_of_element , our_arr):
     our_k_dodat = int(input("our k: "))
     our_arr.reverse()
     index = 0
@@ -28,10 +32,12 @@ def algoritm(count_of_elements ,our_arr):
         index += 1
     our_arr.reverse()
 
-    print(our_arr.index(search_element))
+    print(our_arr.index(search_element))#print index
+
 
 our_arr = []
-count_of_elements = input_num()
+count_of_elements = enter_num()
+input_our_arr(count_of_elements , our_arr)
 input_our_arr(count_of_elements , our_arr)
 algoritm(count_of_elements , our_arr)
 
