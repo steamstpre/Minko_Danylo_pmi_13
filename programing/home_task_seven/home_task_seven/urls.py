d312728ba81359783e4ecfefae407b4562fd1a29
Denial <denialwins6@gmail.com>
2021-11-30 14:01:00 +0200

    PR_7

---
+++ programing/home_task_seven/home_task_seven/urls.py
@@ -0,0 +1,41 @@
+"""home_task_seven URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path , include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tax_free API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.taxfree.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    validators=['ssv', 'flex'],
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/tax/', include('taxfree.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
+    path('redo/c', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
+]
